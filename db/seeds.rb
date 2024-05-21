eac7438ec166c69f5f2523e23cf6262197d0d180
romain chavalle <romain.chavalle@gmail.com>
2024-05-21 16:55:13 +0200

    exo3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+
+puts 'Creating 10 fake articles...'
+10.times do
+  Article.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph)
+end
+puts 'Finished!'
